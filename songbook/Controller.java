c147f6abd44e899e74d7a4e4ca5ea4ac81b5aa07
zumoskala <44268782+zumoskala@users.noreply.github.com>
2018-12-17 23:38:15 +0100

    songbook v2

--- songbook/Controller.java
+++ songbook/Controller.java
@@ -6,17 +6,26 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import songbook.data.Album;
+import songbook.data.Artist;
+import songbook.data.Song;
+import songbook.data.Songbook;
 
 public class Controller implements Initializable{
 	
-	private DataSaver data;
+	private DataSaver data = new DataSaver();
 
 	private Stage stage;
+
+	public Songbook przeczytane;
+
+	private List<Artist> artists;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -37,22 +46,34 @@ public class Controller implements Initializable{
 		
 		File file = fileChooser.showOpenDialog(stage);
 		InputStream is = new FileInputStream(file);
-		//data.readSongbook(is);
+		przeczytane = data.readSongbook(is);
 
 		if(file!=null) {
 			System.out.println(file.getName());
-			System.out.println(is.read());
-			System.out.println(is.read());
-			System.out.println(is.read());
-			System.out.println(is.read()); //sprawdzi³am z ASCII, dobrze czyta zawartoœæ, ale w bajtach
 			
-
-
-
-
 		}
 	}
 
+	public void czytaj() {
+		artists = przeczytane.getArtists();
+		for(Artist artist:artists) {
+			System.out.println("Nowy wykonawca: ");
+			System.out.println(artist.getName());
+			List<Album> albums = artist.getAlbums();
+			for(Album album:albums) {
+				System.out.println("Nazwa albumu: ");
+				System.out.println(album.getName());
+				List<Song> songs = album.getSongs();
+				for(Song song:songs) {
+					System.out.println("Nazwa utworu: ");
+					System.out.println(song.getTitle());
+					System.out.println(song.getLyrics());
+					System.out.println("***");
+				}
+			}
+			
+		}
+	}
 	
 	
 	
